2965a0388f2813f47aae7665be190fc77584b429
OmariMcGinnis <77695012+OmariMcGinnis@users.noreply.github.com>
2021-03-18 14:00:59 -0600

    Add files via upload

--- listAppV1.py
+++ listAppV1.py
@@ -20,16 +20,25 @@ def mainProgram():
             print("Hello, there! Let's work with lists!")
             print("Choose from the following options. Type a number!")
             choice = input("""1. Add to a list or
-2. Return a value at an index
-3. Random Search
-4. Quit Program""")
+2. Add a bunch of numbers to the list
+3. Return a value at an index
+4. Random Search
+5. Linear Search
+6. Print contents of list
+7. Quit Program    """)
             #add a way to catch bad user responses
             if choice == "1":
                 addToList()
             elif choice == "2":
-                indexValues()
+                addMany()
             elif choice == "3":
-                randomSearch
+                indexValues()
+            elif choice == "4":
+                randomSearch()
+            elif choice == "5":
+                linearSearch
+            elif choice == "6":
+                print(myList)
             else:
                 break
         except:
@@ -40,6 +49,14 @@ def addToList():
     newItem = input("Type an integer here!  ")
     myList.append(int(newItem))
 
+def addMany():
+    print("We're gonna adda bunch of integers to your list!")
+    numToAdd = input("How many numbers do you want to add   ")
+    numRange = input("How high do you want the numbers to go?   ")
+    for x in range(0, int(numToAdd)):
+        myList.appent(random.randit(0, int(numRange)))
+    print("Your list is complete")
+
 def indexValues():
     print("Curious about an index position? ME TOO!")
     indexPos = input("What index position would you like to check out?  ")
@@ -49,6 +66,12 @@ def randomSearch():
     print("RaNdOm SeArCh?!?")
     print(myList[random.randint(0, len(myList)-1)])
 
+def linearSearch():
+    print("We're going to search each item one by one!")
+    searchItem = input("What are you looking for...?   ")
+    for x in range(len(yList)):
+        if myList[x] == int(searchItem):
+            print("Your item is at index position {}".format(x))
     
 #dunder main -> Double Underscore---dunder
 if __name__ == "__main__":
